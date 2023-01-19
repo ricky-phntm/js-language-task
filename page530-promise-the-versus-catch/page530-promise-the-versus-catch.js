03447f076d5e795d22ff371c519e8de9da78c5d7
Ricky Lau <ricky@phntms.com>
2023-01-20 09:23:17 +1300

    Add page 530 task

---
+++ page530-promise-the-versus-catch/page530-promise-the-versus-catch.js
@@ -0,0 +1,5 @@
+// Are these code fragments equal? In other words, do they behave the same way in any circumstances, for any handler functions?
+// promise.then(f1).catch(f2);
+// Versus:
+// promise.then(f1, f2);
+// answer, there are different. catch is used to handle error.

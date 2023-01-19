c7ef16df022dcb34e412df434d589fb21af6fb3d
Ricky Lau <ricky@phntms.com>
2023-01-20 06:36:26 +1300

    Add page 468 task

---
+++ page468-class-extends-Object/page468-class-extends-Object.js
@@ -0,0 +1,9 @@
+// Here’s an example of such code (it doesn’t work – why? fix it?):
+// class Rabbit extends Object { 
+//   constructor(name) {
+//     this.name = name; 
+//   }
+// }
+// let rabbit = new Rabbit("Rab");
+// alert( rabbit.hasOwnProperty('name') ); // Error
+super();
